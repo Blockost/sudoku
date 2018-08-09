155b59e2b77137a5461d9f3c2e1bd512004c80a3
ESPIGOLÉ Simon <simon.espigole@gmail.com>
2018-08-09 13:18:13 -0400

    Support for Heroku deployment

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+/*
+ * In production, the application cannot be deployed
+ * using the `ng serve` command of `angular-cli`.
+ * Therefore, the `dist/` (build using `ng build --prod`)
+ * directory needs to be served as static content using
+ *  a standard nodeJS express server
+ */
+app.use(express.static(path.join(__dirname, '/dist/sudoku')));
+
+// Redirect all routes to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/sudoku/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}.`);
+});
