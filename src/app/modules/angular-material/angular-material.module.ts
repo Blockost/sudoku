8b6995a4ffeea4db005f2c8c3508db940625ac9f
Blockost <simon.espigole@gmail.com>
2018-09-30 23:26:06 -0400

    Added tooltp on custom cells with implicit behaviors

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 import { MatBottomSheetModule } from '@angular/material';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  imports: [MatBottomSheetModule, MatListModule, MatIconModule],
-  exports: [MatBottomSheetModule, MatListModule, MatIconModule],
+  imports: [
+    MatBottomSheetModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
+  exports: [
+    MatBottomSheetModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
   declarations: []
 })
 export class AngularMaterialModule {}
